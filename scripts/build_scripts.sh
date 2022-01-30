ea0fb5e11e139a2fb3b709294d59e9f79c44bccc
Asaf Cohen <safhac@gamil.com>
2022-01-30 17:57:19 +0200

    build_packages func runs poetry_init which builds each service wheel

---
+++ scripts/build_scripts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# docker compose up
+function dcu() {
+  docker-compose up --build -d --force-recreate --remove-orphans
+}
+
+# docker build, docker run with network
+function build_run_container() {
+  docker build --pull --rm -t "$1" .
+  docker run --rm -ti --network rabbit_docker_default "$1" "${2:$2}"
+}
+
+function build_packages() {
+  CWD=$PWD
+  services="receiver.service sender.service"
+  for service in $services; do
+    poetry_init $service
+    cd $CWD
+  done
+}
+
+function poetry_init() {
+  cd "./$1"
+  TOMLFILE="pyproject.toml"
+  if [ -f $TOMLFILE ]; then
+    poetry update
+  else
+    poetry init --name '$1' --version '0.1.0' --description '$1' --author 'Asaf Cohen <https://github.com/safhac>'
+  fi
+  poetry lock
+  poetry build
+
+}
