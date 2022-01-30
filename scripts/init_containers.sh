e7677c7766bff7d05dc7ee488736b80584829983
Asaf Cohen <safhac@gamil.com>
2022-01-30 11:21:48 +0200

    remove markings from script

--- scripts/init_containers.sh
+++ scripts/init_containers.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 # docker compose up
-function dcu(){
-    docker-compose up --build -d --force-recreate --remove-orphans
+function dcu() {
+  docker-compose up --build -d --force-recreate --remove-orphans
 }
 
 # docker build, docker run with network
-function build_run_container(){
-    docker build --pull --rm -t "$1" .
-    docker run --rm -ti --network rabbit_docker_default "$1" "${2: $2}"
+function build_run_container() {
+  docker build --pull --rm -t "$1" .
+  docker run --rm -ti --network rabbit_docker_default "$1" "${2:$2}"
 }
 
 function poetry_init() {
   cd ../receiver.service
   RECTOMLFILE=./pyproject.toml
   if test -f "$RECTOMLFILE"; then
-      poetry init
+    poetry init
   else
-      poetry init --name "receiver" --version "0.1.0" --description "subscribe to rabbitmq channel" --author = "Asaf Cohen <https://github.com/safhac>"
+    poetry init --name "receiver" --version "0.1.0" --description "subscribe to rabbitmq channel" --author "Asaf Cohen <https://github.com/safhac>"
   fi
   poetry lock
   cd ../sender.service
   SENSTOMLFILE=./pyproject.toml
   if test -f "$SENSTOMLFILE"; then
-      poetry init
+    poetry init
   else
-      poetry init --name "sender" --version "0.1.0" --description "send messages to rabbitmq" --author = "Asaf Cohen <https://github.com/safhac>"
+    poetry init --name "sender" --version "0.1.0" --description "send messages to rabbitmq" --author "Asaf Cohen <https://github.com/safhac>"
   fi
   poetry lock
 }
@@ -32,4 +32,3 @@ export poetry_init
 export dcu
 export docker_container
 export foo
-
