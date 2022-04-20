40ea95a3565f45b6d7775a02b4d835dd3661c6b8
Asaf Cohen <asafcohen331@gmail.com>
2022-04-20 14:32:13 +0300

    black format source files

--- receiver.service/receiver/receive.py
+++ receiver.service/receiver/receive.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python
 import pika, sys, os
 
+
 def main():
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
     channel = connection.channel()
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue="hello")
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='hello',
-                          auto_ack=True,
-                          on_message_callback=callback)
+    channel.basic_consume(queue="hello", auto_ack=True, on_message_callback=callback)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    print(" [*] Waiting for messages. To exit p ress CTRL+C")
     channel.start_consuming()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
         try:
             sys.exit(0)
         except SystemExit:
--- sender.service/sender/send.py
+++ sender.service/sender/send.py
@@ -2,26 +2,25 @@
 import pika
 from time import sleep
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
-    channel = connection.channel()
-    channel.queue_declare(queue='hello')
 
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+    channel = connection.channel()
+    channel.queue_declare(queue="hello")
 
     for i in range(20):
-        channel.basic_publish(exchange='',
-                              routing_key='hello',
-                              body=f'Hello {i}')
+        channel.basic_publish(exchange="", routing_key="hello", body=f"Hello {i}")
         sleep(1)
 
-    print('closing connection')
+    print("closing connection")
     connection.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
         try:
             sys.exit(0)
         except SystemExit:
